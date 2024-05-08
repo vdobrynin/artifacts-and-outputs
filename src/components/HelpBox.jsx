6454fde6a0fabb4eccec822459fb7ad1338217a0
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-05-07 17:19:25 -0700

    change to h3

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
